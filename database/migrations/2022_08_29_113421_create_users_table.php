6c38aa5f0dcf699d4def8ec5ea1f9047b7bf361d
Mostafa Abdelbrr <mostafa.abdelbrr@hotmail.com>
2022-08-30 23:00:31 +0200

    Increase length of password column data to store hashed password.

--- database/migrations/2022_08_29_113421_create_users_table.php
+++ database/migrations/2022_08_29_113421_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('email', 50)->unique;
-            $table->string('password', 50);
+            $table->string('password', 100);
             $table->string('name', 50);
             $table->string('faculty', 50);
             $table->string('faculty_department', 50);
